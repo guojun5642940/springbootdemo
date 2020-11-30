a99647db75a2700cd48ccab85a5a19debfb3940c
郭俊 <guoj65@chinaunicom.cn>
2020-11-30 10:42:35 +0800

    11.30

--- src/main/java/com/guoj/SpringbootdemoApplication.java
+++ src/main/java/com/guoj/SpringbootdemoApplication.java
@@ -2,13 +2,20 @@ package com.guoj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class SpringbootdemoApplication {
+@EnableDiscoveryClient
+public class SpringbootdemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootdemoApplication.class);
+	}
 }
